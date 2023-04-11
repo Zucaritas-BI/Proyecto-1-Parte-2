2837371d0468c405d456f3f7b26e8ed8abb7d865
Daniel Aguilera <d.aguilera@uniandes.edu.co>
2023-04-11 15:53:12 -0500

    change to fast api

--- backend/main.py
+++ backend/main.py
@@ -1,18 +1,8 @@
-from flask import Flask
-from waitress import serve
-import socket
-app = Flask(__name__)
+from fastapi import FastAPI
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+app = FastAPI()
 
-if __name__ == '__main__':
-    # host = '0.0.0.0'
-    # print("\033[1;32;40m Server started \033[0;37;40m")
-    # host_private = socket.gethostbyname(socket.gethostname())
-    # print(f"\033[1;32;40m Running on http://{host_private}:5000 \033[0;37;40m")
-    # serve(app, host=host, port=5000)
-    # print("\033[1;32;40m Server stopped \033[0;37;40m")
-    app.run()
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
 

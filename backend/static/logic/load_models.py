d20f968e8cb4fc2c5e6f67ce1e61429f114042ba
Daniel Aguilera <d.aguilera@uniandes.edu.co>
2023-04-15 22:05:16 -0500

    done

--- backend/static/logic/load_models.py
+++ backend/static/logic/load_models.py
@@ -1,6 +1,40 @@
-import pickle
+from sklearn.neural_network import MLPClassifier
+from sklearn.pipeline import Pipeline
+import joblib
+from sklearn.model_selection import train_test_split
+from text_pipeline import TextProcessor
+import pandas as pd
+import nltk
 
-def load_model():
-    with open('./models/model.pkl', 'rb') as f:
-        model = pickle.load(f)
-    return model
+def tokenize(text):
+    tokenizer = nltk.TweetTokenizer()
+    return tokenizer.tokenize(text)
+
+vectorizer = joblib.load('backend/static/logic/datos/CountVectorizer.pkl')
+
+pipeline = Pipeline([
+    ('preprocessor', TextProcessor()),
+    ('vectorizer', vectorizer),
+    ('classifier', MLPClassifier(verbose=True, early_stopping=True, random_state=42))
+])
+
+# Cargamos el dataset
+df_peliculas = pd.read_csv('backend/static/logic/datos/MovieReviewsProcesado.csv')
+
+# Remove Unnamed: 0 column
+df_peliculas = df_peliculas.drop(columns=['Unnamed: 0'])
+
+# Split dataset into train and test
+X_train, X_test, y_train, y_test = train_test_split(df_peliculas["processed_text"], df_peliculas['sentimiento'], test_size=0.3, random_state=42)
+
+# Train model
+pipeline.fit(X_train, y_train)
+
+# Save model
+joblib.dump(pipeline, 'backend/static/logic/datos/MLPClassifier.pkl')
+
+# Test model
+print(pipeline.score(X_test, y_test))
+
+# Lets test with some random text
+print(pipeline.predict(['Me encantó la película, es muy buena']))
--- backend/static/logic/text_pipeline.py
+++ backend/static/logic/text_pipeline.py
@@ -1,32 +1,66 @@
 from sklearn.base import BaseEstimator, TransformerMixin
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
+from nltk.stem import SnowballStemmer
 import string
+import re
+import inflect
+import spacy
+
 
 class TextProcessor(BaseEstimator, TransformerMixin):
-    def __init__(self): 
-        self.stop_words = set(stopwords.words('english'))
-        
+    def __init__(self):
+        self.stop_words = set(stopwords.words('spanish') + list(string.punctuation) + list('¿¡'))
+        self.stemmer = SnowballStemmer('spanish')
+        self.inflect = inflect.engine()
+        self.nlp = spacy.load("es_core_news_sm")
+
     def fit(self, X, y=None):
         return self
-        
+
     def transform(self, X):
         # tokenize text
         tokenized_text = [word_tokenize(text) for text in X]
-        # remove stop words and punctuation
-        processed_text = []
-        for tokens in tokenized_text:
-            processed_tokens = []
-            for token in tokens:
-                if token not in self.stop_words and token not in string.punctuation:
-                    processed_tokens.append(token.lower())
-            processed_text.append(processed_tokens)
-        return processed_text
-        
-from sklearn.pipeline import Pipeline
-from sklearn.feature_extraction.text import CountVectorizer
+        # preprocess text
+        processed_text = [self.preprocess_text(tokens) for tokens in tokenized_text]
+        # Lemmatize text
+        lemmatized_text = [self.lemmatize_words(tokens) for tokens in processed_text]
+        # stem words
+        stemmed_text = [self.stem_words(tokens) for tokens in lemmatized_text]
+        # join tokens into a single string for each text
+        return [' '.join(tokens) for tokens in stemmed_text]
 
-pipeline = Pipeline([
-    ('text_processor', TextProcessor()),
-    ('vectorizer', CountVectorizer())
-])
+    def preprocess_text(self, tokens):
+        processed_tokens = []
+        for token in tokens:
+            # convert all characters to lowercase
+            token = self.convert_to_lowercase(token)
+            # remove special characters and spanish tilde
+            token = self.remove_special_characters(token)
+            # replace numbers with words
+            token = self.replace_numbers(token)
+            # remove stop words and replace alphanumeric characters
+            if self.is_valid_token(token):
+                processed_tokens.append(token)
+        return processed_tokens
+
+    def convert_to_lowercase(self, token):
+        return token.lower()
+
+    def remove_special_characters(self, token):
+        return re.sub(r'[^a-z0-9\s]', '', token).replace('á', 'a').replace('é', 'e').replace('í', 'i').replace('ó', 'o').replace('ú', 'u')
+
+    def replace_numbers(self, token):
+        if token.isdigit():
+            return self.inflect.number_to_words(token)
+        else:
+            return token
+
+    def stem_words(self, tokens):
+        return [self.stemmer.stem(token) for token in tokens]
+
+    def lemmatize_words(self, tokens):
+        return [token.lemma_ for token in self.nlp(' '.join(tokens))]
+
+    def is_valid_token(self, token):
+        return token not in self.stop_words and token.isalnum()
